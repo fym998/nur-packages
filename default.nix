807677c04bf2449395957d92790cfa641448de71
FU Yiming <fujun998@outlook.com>
2025-05-26 19:30:15 +0800

    add bitsrun-rs

--- default.nix
+++ default.nix
@@ -15,6 +15,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
+  bitsrun-rs = pkgs.callPackage ./pkgs/bitsrun-rs { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
