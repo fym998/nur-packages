9aa6788eb90006bef95707cdaa2eead1b00dd3b0
fym998 <fujun998@outlook.com>
2025-05-31 09:41:43 +0800

    restrict libfprint-fpcmoh and to x86_64-linux

--- default.nix
+++ default.nix
@@ -5,10 +5,11 @@
 # Having pkgs default to <nixpkgs> is fine though, and it lets you use short
 # commands such as:
 #     nix-build -A mypackage
-{pkgs ? import <nixpkgs> {}, pkgs-libfprint-1_94_6}:
-  let
-    libfprint-fpcmoh = pkgs-libfprint-1_94_6.callPackage ./pkgs/libfprint-fpcmoh {};
-  in{
+{pkgs ? import <nixpkgs> {}, pkgs-libfprint-1_94_6, system ? builtins.currentSystem}:
+let
+  libfprint-fpcmoh = pkgs-libfprint-1_94_6.callPackage ./pkgs/libfprint-fpcmoh {};
+  isX86_64Linux = system == "x86_64-linux";
+in {
   # The `lib`, `modules`, and `overlays` names are special
   lib = import ./lib {inherit pkgs;}; # functions
   modules = import ./modules; # NixOS modules
@@ -16,8 +17,13 @@
 
   example-package = pkgs.callPackage ./pkgs/example-package {};
   bitsrun-rs = pkgs.callPackage ./pkgs/bitsrun-rs {};
-  inherit libfprint-fpcmoh;
-  fprintd-fpcmoh = pkgs-libfprint-1_94_6.fprintd.override { libfprint = libfprint-fpcmoh; };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
-}
+} //
+(
+  if isX86_64Linux then {
+    inherit libfprint-fpcmoh;
+    fprintd-fpcmoh = pkgs-libfprint-1_94_6.fprintd.override {
+      libfprint = libfprint-fpcmoh;
+    };
+  }
+  else {}
+)
