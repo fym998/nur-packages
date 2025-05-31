c8af2bf526d001ce12d56c73b836a8e06afccce0
fym998 <fujun998@outlook.com>
2025-05-31 10:06:32 +0800

    设置 pkgs-libfprint-1_94_6 的默认值

--- default.nix
+++ default.nix
@@ -5,25 +5,30 @@
 # Having pkgs default to <nixpkgs> is fine though, and it lets you use short
 # commands such as:
 #     nix-build -A mypackage
-{pkgs ? import <nixpkgs> {}, pkgs-libfprint-1_94_6, system ? builtins.currentSystem}:
-let
+{
+  pkgs ? import <nixpkgs> {},
+  pkgs-libfprint-1_94_6 ? builtins.fetchTree "github:NixOS/nixpkgs/c1f26cac27c78942f0e61a1fff6cdc4a63f02960",
+  system ? builtins.currentSystem,
+}: let
   libfprint-fpcmoh = pkgs-libfprint-1_94_6.callPackage ./pkgs/libfprint-fpcmoh {};
   isX86_64Linux = system == "x86_64-linux";
-in {
-  # The `lib`, `modules`, and `overlays` names are special
-  lib = import ./lib {inherit pkgs;}; # functions
-  modules = import ./modules; # NixOS modules
-  overlays = import ./overlays; # nixpkgs overlays
+in
+  {
+    # The `lib`, `modules`, and `overlays` names are special
+    lib = import ./lib {inherit pkgs;}; # functions
+    modules = import ./modules; # NixOS modules
+    overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package {};
-  bitsrun-rs = pkgs.callPackage ./pkgs/bitsrun-rs {};
-} //
-(
-  if isX86_64Linux then {
-    inherit libfprint-fpcmoh;
-    fprintd-fpcmoh = pkgs-libfprint-1_94_6.fprintd.override {
-      libfprint = libfprint-fpcmoh;
-    };
+    example-package = pkgs.callPackage ./pkgs/example-package {};
+    bitsrun-rs = pkgs.callPackage ./pkgs/bitsrun-rs {};
   }
-  else {}
-)
+  // (
+    if isX86_64Linux
+    then {
+      inherit libfprint-fpcmoh;
+      fprintd-fpcmoh = pkgs-libfprint-1_94_6.fprintd.override {
+        libfprint = libfprint-fpcmoh;
+      };
+    }
+    else {}
+  )
