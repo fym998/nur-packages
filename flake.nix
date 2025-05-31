9c74862b68c610309631fc0351ffe5b87383d629
fym998 <fujun998@outlook.com>
2025-05-31 16:47:10 +0800

    Refactor flake.nix to include flake-utils in outputs and streamline package imports

--- flake.nix
+++ flake.nix
@@ -3,16 +3,34 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs-libfprint-1_94_6.url = "github:nixos/nixpkgs/c1f26cac27c78942f0e61a1fff6cdc4a63f02960";
+    
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, nixpkgs-libfprint-1_94_6 }:
+  outputs = { self, nixpkgs, nixpkgs-libfprint-1_94_6, flake-utils }:
+    flake-utils.lib.eachDefaultSystem ( system:
     let
-      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
-    in
-    {
-      legacyPackages = forAllSystems (system: import ./default.nix {
+      result = import ./default.nix {
+        inherit system;
         pkgs = import nixpkgs { inherit system; };
-        pkgs-libfprint-1_94_6 = import nixpkgs-libfprint-1_94_6 { inherit system; };
-      });
-      packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
-    };
+        pkgs-libfprint-1_94_6 = 
+          if system == "x86_64-linux" then
+            import nixpkgs-libfprint-1_94_6 { inherit system; }
+          else null;
+      };
+      packagesForx86_64Linux =
+        if system == "x86_64-linux" then
+          let
+            libfprint-fpcmoh = result.libfprint-fpcmoh;
+            fprintd-fpcmoh = result.fprintd-fpcmoh;
+          in {
+            inherit libfprint-fpcmoh fprintd-fpcmoh;
+          }
+        else {};
+      packages = {
+        bitsrun-rs = result.bitsrun-rs;
+      } // packagesForx86_64Linux;
+    in {
+      inherit packages;
+    }
+  );
 }
