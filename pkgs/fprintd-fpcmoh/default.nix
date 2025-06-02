f207b689ed9a7fbf7ee1851df2130cf35817b93d
fym998 <fujun998@outlook.com>
2025-06-02 20:31:37 +0800

    Fix fprintd-fpcmoh package definition

--- pkgs/fprintd-fpcmoh/default.nix
+++ pkgs/fprintd-fpcmoh/default.nix
@@ -1,17 +1,15 @@
 { pkgs, ... }:
-{
-  fprintd-fpcmoh =
-    pkgs.fprintd.overrideAttrs
-      (finalAttrs: {
-        pname = "fprintd-fpcmoh";
-        version = "1.94.4";
-        src = pkgs.fetchFromGitLab {
-          domain = "gitlab.freedesktop.org";
-          owner = "libfprint";
-          repo = "fprintd";
-          rev = "v${finalAttrs.version}";
-          sha256 = "sha256-B2g2d29jSER30OUqCkdk3+Hv5T3DA4SUKoyiqHb8FeU=";
-        };
-      }).override
-      { libfprint = pkgs.callPackage ../libfprint-fpcmoh { }; };
-}
+let
+  fprintd-1_94_4 = pkgs.fprintd.overrideAttrs (finalAttrs: {
+    pname = "fprintd-fpcmoh";
+    version = "1.94.4";
+    src = pkgs.fetchFromGitLab {
+      domain = "gitlab.freedesktop.org";
+      owner = "libfprint";
+      repo = "fprintd";
+      rev = "v${finalAttrs.version}";
+      sha256 = "sha256-B2g2d29jSER30OUqCkdk3+Hv5T3DA4SUKoyiqHb8FeU=";
+    };
+  });
+in
+fprintd-1_94_4.override { libfprint = pkgs.callPackage ../libfprint-fpcmoh { }; }
