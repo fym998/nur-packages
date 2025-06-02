9bacccf63b2ef934457808347a5b2a073408ac9f
fym998 <fujun998@outlook.com>
2025-06-02 23:04:53 +0800

    Add treefmt-nix integration and update pre-commit checks

--- flake.nix
+++ flake.nix
@@ -5,8 +5,14 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     flake-utils.url = "github:numtide/flake-utils";
+
     pre-commit-hooks = {
       url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -27,26 +33,36 @@
       nixpkgs,
       flake-utils,
       pre-commit-hooks,
+      treefmt-nix,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
-        result = import ./default.nix { inherit system pkgs; };
+
+        # Eval the treefmt modules from ./treefmt.nix
+        treefmtEval.${system} = treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs.nixfmt.enable = true;
+          programs.deadnix.enable = true;
+        };
+
       in
       {
         legacyPackages = import ./default.nix { pkgs = import nixpkgs { inherit system; }; };
         packages = nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system};
 
-        formatter = pkgs.nixfmt-rfc-style;
+        formatter = treefmtEval.${system}.config.build.wrapper;
 
         checks = {
           pre-commit-check = pre-commit-hooks.lib.${system}.run {
             src = ./.;
             hooks = {
               nixfmt-rfc-style.enable = true;
+              deadnix.enable = true;
             };
           };
+          # formatting = treefmtEval.${pkgs.system}.config.build.check self;
         };
 
         devShells = {
