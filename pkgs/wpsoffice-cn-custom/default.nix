dfb264d6a5ab3bc806d962412aa3a6327cf03a8a
fym998 <fujun998@outlook.com>
2025-06-06 00:04:04 +0800

    新增wpsoffice-cn-custom包，包含自定义postInstall脚本以支持fcitx和ibus输入法

---
+++ pkgs/wpsoffice-cn-custom/default.nix
@@ -0,0 +1,14 @@
+{
+  wpsoffice-cn,
+}:
+wpsoffice-cn.overrideAttrs (previousAttrs: {
+  pname = "wpsoffice-cn-custom";
+  postInstall =
+    (previousAttrs.postInstall or "")
+    + ''
+      for i in $out/share/applications/*;do
+        substituteInPlace $i \
+          --replace Exec= "Exec=env XMODIFIERS=\"@im=fcitx\" GTK_IM_MODULE=\"fcitx\" QT_IM_MODULE=\"fcitx\" SDL_IM_MODULE=fcitx GLFW_IM_MODULE=ibus"
+      done
+    '';
+})
