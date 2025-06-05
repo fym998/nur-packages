60dc336886e2313cf9af6e97117dfd6fbfd2b395
fym998 <fujun998@outlook.com>
2025-06-05 23:59:57 +0800

    更新VSCode设置以启用Nix LSP；在flake.nix中添加nil包到开发环境

--- flake.nix
+++ flake.nix
@@ -71,6 +71,10 @@
           default = pkgs.mkShellNoCC {
             inherit (self.checks.${system}.pre-commit-check) shellHook;
             buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+            packages = with pkgs; [
+              nil
+              # nixd
+            ];
           };
         };
       }
