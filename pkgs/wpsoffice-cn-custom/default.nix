3f27ec28f2b2804b540fffd45b6097a8f0d4dfba
fym998 <fujun998@outlook.com>
2025-06-06 00:35:17 +0800

    修复wpsoffice-cn-custom包的postInstall脚本，确保desktop中Exec命令格式正确

--- pkgs/wpsoffice-cn-custom/default.nix
+++ pkgs/wpsoffice-cn-custom/default.nix
@@ -8,7 +8,7 @@ wpsoffice-cn.overrideAttrs (previousAttrs: {
     + ''
       for i in $out/share/applications/*;do
         substituteInPlace $i \
-          --replace Exec= "Exec=env XMODIFIERS=\"@im=fcitx\" GTK_IM_MODULE=\"fcitx\" QT_IM_MODULE=\"fcitx\" SDL_IM_MODULE=fcitx GLFW_IM_MODULE=ibus"
+          --replace Exec= "Exec=env XMODIFIERS=\"@im=fcitx\" GTK_IM_MODULE=\"fcitx\" QT_IM_MODULE=\"fcitx\" SDL_IM_MODULE=fcitx GLFW_IM_MODULE=ibus "
       done
     '';
 })
