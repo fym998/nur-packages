a904202cc995032a29edac2e87a27ee00b65754a
fym998 <fujun998@outlook.com>
2025-06-06 23:20:13 +0800

    fix: update postInstall script to use wrapProgram

--- pkgs/wpsoffice-cn-fcitx/default.nix
+++ pkgs/wpsoffice-cn-fcitx/default.nix
@@ -6,12 +6,17 @@ wpsoffice-cn.overrideAttrs (previousAttrs: {
   postInstall =
     (previousAttrs.postInstall or "")
     + ''
-      for i in $out/share/applications/*;do
-        substituteInPlace $i \
-          --replace Exec= "Exec=env XMODIFIERS=\"@im=fcitx\" GTK_IM_MODULE=\"fcitx\" QT_IM_MODULE=\"fcitx\" SDL_IM_MODULE=fcitx GLFW_IM_MODULE=ibus "
+      for exe in $out/bin/*;do
+        wrapProgram $exe \
+          --prefix XMODIFIERS : \"@im=fcitx\"
+          --prefix GTK_IM_MODULE : \"fcitx\"
+          --prefix QT_IM_MODULE : \"fcitx\"
+          --prefix SDL_IM_MODULE : fcitx
+          --prefix GLFW_IM_MODULE : ibus
       done
     '';
   meta = previousAttrs.meta // {
     description = "WPS Office CN with Fcitx support";
+    mainProgram = "wps";
   };
 })
