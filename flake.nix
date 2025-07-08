4e42001715e15e839ed2766c4ed0f209203246b6
fym998 <61316972+fym998@users.noreply.github.com>
2025-07-08 22:40:54 +0800

    chore: update flake-parts.inputs.nixpkgs-lib to reuse nixpkgs

--- flake.nix
+++ flake.nix
@@ -4,7 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
 
     systems.url = "github:nix-systems/default-linux";
   };
