a85f169b3c0b63e916a4781fb3d8f70b92c968c1
fym998 <61316972+fym998@users.noreply.github.com>
2025-07-11 14:24:56 +0800

    fix: correct overlay definition to include legacyPackages

--- overlay.nix
+++ overlay.nix
@@ -1,4 +1,5 @@
 # You can use this file as a nixpkgs overlay. This is useful in the
 # case where you don't want to add the whole NUR namespace to your
 # configuration.
-_self: super: import ./pkgs { pkgs = super; }
+_self: super: _self: super:
+(import ./pkgs { pkgs = super; }).legacyPackages
