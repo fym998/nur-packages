4e0cb693c59fc6d1c1c49213b676b728a1222b62
fym998 <61316972+fym998@users.noreply.github.com>
2025-07-12 02:55:07 +0800

    optimize: avoid using newScope in lib.packagesFromDirectoryRecursive

--- pkgs.nix
+++ pkgs.nix
@@ -4,11 +4,16 @@
   ...
 }:
 let
-  legacyPackages = lib.packagesFromDirectoryRecursive {
-    inherit (pkgs) callPackage newScope;
-    directory = ./pkgs;
-  };
-
+  legacyPackages =
+    lib.packagesFromDirectoryRecursive {
+      inherit (pkgs) callPackage;
+      directory = ./pkgs;
+    }
+    // {
+      fprintd-fpcmoh = pkgs.callPackage ./pkgs/fprintd-fpcmoh/package.nix {
+        inherit (legacyPackages) libfprint-fpcmoh;
+      };
+    };
 in
 {
   inherit legacyPackages;
