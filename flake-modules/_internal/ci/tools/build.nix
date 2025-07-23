c5f653474447c0473b1ec8bdcb21e1ab43079540
fym998 <61316972+fym998@users.noreply.github.com>
2025-07-23 22:49:45 +0800

    chore(ci/tools/build): 只有当 CACHIX_AUTH_TOKEN 存在且非空时，才添加 --cachix-cache 参数

--- flake-modules/_internal/ci/tools/build.nix
+++ flake-modules/_internal/ci/tools/build.nix
@@ -1,16 +1,15 @@
-{ writeShellApplication, nix-fast-build }:
-writeShellApplication {
-  name = "build";
+{ writeShellScriptBin, nix-fast-build }:
+writeShellScriptBin "build" ''
+  args=(
+    --flake ".#ciPackages.$(nix eval --raw --impure --expr builtins.currentSystem)"
+    --no-nom
+    --skip-cached
+  )
 
-  runtimeInputs = [
-    nix-fast-build
-  ];
+  # 只有当 CACHIX_AUTH_TOKEN 存在且非空时，才添加 --cachix-cache 参数
+  if [ -n "''${CACHIX_AUTH_TOKEN:-}" ]; then
+    args+=(--cachix-cache "''${CACHIX_CACHE}")
+  fi
 
-  text = ''
-    nix-fast-build \
-      --flake ".#ciPackages.$(nix eval --raw --impure --expr builtins.currentSystem)" \
-      --no-nom \
-      --skip-cached \
-      --cachix-cache "$CACHIX_CACHE"
-  '';
-}
+  ${nix-fast-build}/bin/nix-fast-build "''${args[@]}"
+''
