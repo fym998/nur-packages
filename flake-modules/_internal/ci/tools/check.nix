0684ee576cbadc22a757f04207dd537ed0c765e9
fym998 <61316972+fym998@users.noreply.github.com>
2025-11-03 00:08:03 +0800

    chore: fix check script

--- flake-modules/_internal/ci/tools/check.nix
+++ flake-modules/_internal/ci/tools/check.nix
@@ -1,4 +1,6 @@
 { nix-fast-build, writeShellScriptBin }:
 writeShellScriptBin "check" ''
-  ${nix-fast-build} --skip-cached --no-nom --flake ".#checks.$(nix eval --raw --impure --expr builtins.currentSystem)"
+  ${nix-fast-build}/bin/nix-fast-build \
+    --skip-cached --no-nom \
+    --flake ".#checks.$(nix eval --raw --impure --expr builtins.currentSystem)"
 ''
