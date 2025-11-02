9700a083fbf086ddbc5437e5b8598a6f167a87d7
fym998 <61316972+fym998@users.noreply.github.com>
2025-11-03 00:00:30 +0800

    refactor: move flattenPkgs to flake.nix

--- flake.nix
+++ flake.nix
@@ -60,10 +60,30 @@
                 allowUnsupportedSystem = true;
               };
             };
-            ciPackages = lib.filterAttrs (name: _p: !lib.hasPrefix "_" name) self'.packages;
+
+            legacyPackages = import ./pkgs { inherit pkgs; };
+
+            packages =
+              let
+                # from https://github.com/drupol/pkgs-by-name-for-flake-parts/blob/main/flake-module.nix
+                flattenPkgs =
+                  separator: path: value:
+                  if lib.isDerivation value then
+                    {
+                      ${lib.concatStringsSep separator path} = value;
+                    }
+                  else if lib.isAttrs value then
+                    lib.concatMapAttrs (name: flattenPkgs separator (path ++ [ name ])) value
+                  else
+                    # Ignore the functions which makeScope returns
+                    { };
+              in
+              flattenPkgs "." [ ] self'.legacyPackages;
+
+            ciPackages = self'.packages;
+
             update.packages = self'.packages;
-          }
-          // import ./pkgs { inherit pkgs; };
+          };
       }
     );
 }
